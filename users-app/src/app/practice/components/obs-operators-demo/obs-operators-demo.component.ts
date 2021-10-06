c65822213943e74bdf576567bc58025d2278b2f8
Sumit K <synergy2411@outlook.com>
2021-10-06 15:00:11 +0530

    filter operators

--- users-app/src/app/practice/components/obs-operators-demo/obs-operators-demo.component.ts
+++ users-app/src/app/practice/components/obs-operators-demo/obs-operators-demo.component.ts
@@ -1,6 +1,8 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { from, of, interval, range, fromEvent,  } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
+import { take, map, pluck, filter, reduce, takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-obs-operators-demo',
@@ -10,8 +12,14 @@ import { ajax } from 'rxjs/ajax';
 export class ObsOperatorsDemoComponent implements OnInit, AfterViewInit {
 
   @ViewChild("btn") btn : ElementRef<HTMLButtonElement>;
+  searchForm : FormGroup;
+  search = new FormControl();
 
-  constructor() { }
+  constructor(private fb : FormBuilder) {
+    this.searchForm = this.fb.group({
+      search : this.search
+    })
+   }
 
   ngAfterViewInit(){
     fromEvent(this.btn.nativeElement, 'click').subscribe(response => {
@@ -21,6 +29,47 @@ export class ObsOperatorsDemoComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit(): void {
+
+    this.search.valueChanges.
+      pipe(
+        takeWhile((value)=>{
+          console.log("[TAKE WHILE]", value)
+          return !value.includes('foo');
+        })
+      ).
+    subscribe(console.log);
+
+    const users = [
+      {email : "test1@test.com", age : 32},
+      {email : "test2@test.com", age : 33},
+      {email : "test3@test.com", age : 34},
+      {email : "test4@test.com", age : 35},
+    ]
+
+    const numbers = [2,3,4,5,6];
+
+
+
+    // reduce
+    // const source = from(numbers)
+    // source.pipe(
+    //   reduce((acc, curr)=>{
+    //     return acc + curr
+    //   })
+    // ).subscribe(console.log)
+
+    // pluck & filter
+    // const source = from(users)
+    // source.pipe(
+    //   pluck("age"),
+    //   filter(age => age > 33)
+    // ).subscribe(console.log)
+
+
+    // interval(1000).pipe(
+    //   take(5),
+    //   map(val => val*2)
+    // ).subscribe(console.log)
 
     // AJAX Calls
     // ajax.getJSON('https://api.github.com/users')
