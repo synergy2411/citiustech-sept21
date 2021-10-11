7bd54ce646adcf03b5c6c99dc6618beb5c6d5ce9
Sumit K <synergy2411@outlook.com>
2021-10-11 12:08:28 +0530

    multi modules

--- users-app/src/app/modules/lazy/lazy.module.ts
+++ users-app/src/app/modules/lazy/lazy.module.ts
@@ -2,17 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IAmLazyComponent } from './i-am-lazy/i-am-lazy.component';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 const LAZY_ROUTES : Routes = [
   {
     path : '',            // http://localhost:4200/lazy
-    component : IAmLazyComponent
+    component : IAmLazyComponent,
+    children : [{
+      path : 'home',       // http://localhost:4200/lazy/home - Eagerly loaded in Lazy module
+      component : HomeComponent
+    },{
+      path : 'dashboard',   // http://localhost:4200/lazy/dashboard - Lazily loaded in Lazy module
+      loadChildren : () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)
+    },{
+      path : 'patient',     // http://localhost:4200/lazy/patient - Lazily loaded in Lazy module
+      loadChildren : () => import('./modules/patient/patient.module').then(m => m.PatientModule)
+    }]
   }
 ]
 
 @NgModule({
   declarations: [
-    IAmLazyComponent
+    IAmLazyComponent, HomeComponent
   ],
   imports: [
     CommonModule,
